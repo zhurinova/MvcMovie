340ed547441e758c5af2438fc3ce97347b3f9f9f
Мария <@DESKTOP-HVCH639>
2024-09-22 22:16:22 +0300

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]  //чтобы Entity Framework Core мог правильно сопоставлять Price валюту в базе данных
+    public decimal Price { get; set; }
+}
